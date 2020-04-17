42865b365a496521577704ee87e32ee76da206a0
Casey Clarke <casey.clarke@ncfbins.com>
2020-04-17 12:18:44 -0400

    Dance Dance

--- src/app/metrics/metrics.component.ts
+++ src/app/metrics/metrics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Processes } from '../processes/processNames';
 
 @Component({
   selector: 'app-metrics',
@@ -7,25 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MetricsComponent implements OnInit {
 
-  @Input() heapSizeValue: string;
-  @Input() heapSizeUnit: string;
-  @Input() heapUsageValue: string;
-  @Input() heapUsageUnit: string;
-  @Input() usedHeapSizeValue: string;
-  @Input() usedHeapSizeUnit: string;
-  @Input() activeRequestsValue: string;
-  @Input() activeHandelsValue: string;
-  @Input() eventLoopLatencyValue: string;
-  @Input() eventLoopLatencyUnit: string;
-  @Input() eventLoopLatencyP95Value: string;
-  @Input() eventLoopLatencyP95Unit: string;
-  @Input() httpMeanLatencyValue: string;
-  @Input() httpMeanLatencyUnit: string;
-  @Input() httpP95LatencyValue: string;
-  @Input() httpP95LatencyUnit: string;
-  @Input() httpValue: string;
-  @Input() httpUnit: string;
-
+  @Input() jlist: Processes;
+  
   constructor() { }
 
   ngOnInit(): void {
